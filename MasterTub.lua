6941639a398f32d369ae38cfdaa705e4bdadc905
iiBlackLotus <88800521+iiBlackLotus@users.noreply.github.com>
2022-04-14 02:09:03 +0100

    Update MasterTub.lua

--- MasterTub.lua
+++ MasterTub.lua
@@ -455,6 +455,8 @@ function Library:Window(WindowName, GameName, VersionNumber)
 			end)
 			DropdownTitle.FocusLost:Connect(function()
 				isFocused = false
+				local t, c = search(" ")
+				TweenService:Create(DropdownTemplate, TweenInfo.new(.8, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),{Size = UDim2.new(.9,0,0,(c*26)+36)}):Play()
 			end)
 			
 			DropdownTitle:GetPropertyChangedSignal("Text"):Connect(function(String)
@@ -466,8 +468,7 @@ function Library:Window(WindowName, GameName, VersionNumber)
 					local SearchTable, count = search(DropdownTitle.Text)
 					TweenService:Create(DropdownTemplate, TweenInfo.new(.8, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),{Size = UDim2.new(.9,0,0,(count*26)+36)}):Play()
 				else
-					local t, c = search(" ")
-					TweenService:Create(DropdownTemplate, TweenInfo.new(.8, Enum.EasingStyle.Quad, Enum.EasingDirection.Out),{Size = UDim2.new(.9,0,0,(c*26)+36)}):Play()
+					
 				end
 			end)
 
